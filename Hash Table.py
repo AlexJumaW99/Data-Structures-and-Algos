c2763c6f23326f0b97563e561e0ce7f6bba87a0a
Alex Juma <50864725+AlexJumaW99@users.noreply.github.com>
2022-03-09 23:18:21 +0300

    Add files via upload

---
+++ Hash Table.py
@@ -0,0 +1,32 @@
+#Disclaimer: Does not handle collisions
+
+class Hash_Table:
+    def __init__(self):
+        self.MAX = 100
+        self.arr = [None for i in range(self.MAX)]
+
+    def get_hash(self, key):
+        h = 0
+        for char in key:
+            h += ord(char)
+        return h % self.MAX
+    
+    def __setitem__(self, key, val):
+        h = self.get_hash(key)
+        self.arr[h] = val
+
+    def __getitem__(self, key):
+        h = self.get_hash(key)
+        return self.arr[h]
+    
+    def __delitem__(self, key):
+        h = self.get_hash(key)
+        self.arr[h] = None
+
+h1 = Hash_Table()
+h1['alex'] = 90
+print(h1['alex']) 
+
+del h1['alex']
+print(h1['alex']) 
+
